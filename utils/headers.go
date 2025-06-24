04b455320a1d7d2df3903c2b6e74b0160f1cc72f
Shreya Shrestha <shreyashrestha2k57@gmail.com>
2025-06-24 14:01:14 +0545

    necessary comments added

--- utils/headers.go
+++ utils/headers.go
@@ -7,6 +7,8 @@ import (
 
 func Heading(heading string, number string) {
 	var length = len(heading) + 10
+
+	// "\033[1m ... \033[0m" bolds text in terminal
 	var headingText = fmt.Sprintf("\033[1m**-- %s --**\033[0m", heading)
 	fmt.Println(headingText)
 	if number == "1" {
