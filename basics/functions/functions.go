bb21153d0db85b5f35579298d2bba126436f03d4
Shreya Shrestha <shreyashrestha2k57@gmail.com>
2025-07-08 22:58:41 +0545

    functons in go

--- basics/functions/functions.go
+++ basics/functions/functions.go
@@ -5,14 +5,60 @@ import (
 	"go_lang/utils"
 )
 
-func functions(paramter1, parameter2 int, paramter4 string) (int, int) {
-	result1 := paramter1 + parameter2
-	result2 := len(paramter4)
-	return result1, result2
+func funcWithNoParameters() {
+	utils.FormatOutput("Functions", "No parameters", "This function does not take any parameters and does not return any values.")
+}
+
+func functWithSameParameters(paramter1, paramter2 int) int {
+	return1 := paramter1 + paramter2
+	return return1
+}
+
+func functWithTwoReturns(parameter1, parameter2 int) (int, string) {
+	if parameter2 == 0 {
+		return 0, "Cannot divide by zero"
+	}
+	return parameter1 / parameter2, ""
+}
+
+func funcWithNamedReturn(parameter1, parameter2 int) (result int, err string) {
+	if parameter2 == 0 {
+		err = "Cannot divide by zero"
+		result = 0
+	} else {
+		err = "Success"
+		result = parameter1 / parameter2
+	}
+	return
+}
+
+func funcWithNameReturnShadow() (return1 int) {
+	return1 = 10
+	if true {
+		return1 := 20
+		fmt.Println("Inner return1:", return1) // This will print the inner return1
+	}
+	fmt.Println("Outer return1:", return1) // This will print the outer return1
+	return
 }
 
 func Functions() {
-	result1, result2 := functions(10, 20, "Hello World")
 	utils.Heading("Functions", "1")
-	utils.FormatOutput("Functions", fmt.Sprintf("paramter1: %d, parameter2: %d, paramter3: %s", 10, 20, "Hello world"), fmt.Sprintf("result1: %d, result2: %d", result1, result2))
+	paramter1 := 20
+	paramter2 := 10
+	funcWithNoParameters()
+	add := functWithSameParameters(paramter1, paramter2)
+	utils.FormatOutput("Functions With same type paramters", fmt.Sprintf("paramter1: %d, parameter2: %d", paramter1, paramter2), fmt.Sprintf("result of function: %d", add))
+	quotient, err := functWithTwoReturns(paramter1, paramter2)
+	if err != "" {
+		utils.FormatOutput("Functions with two returns", fmt.Sprintf("parameter1: %d, parameter2: %d", paramter1, paramter2), fmt.Sprintf("error: %s", err))
+	} else {
+		utils.FormatOutput("Functions with two returns", fmt.Sprintf("parameter1: %d, parameter2: %d", paramter1, paramter2), fmt.Sprintf("result of function: %d", quotient))
+	}
+
+	value, err := funcWithNamedReturn(paramter1, paramter2)
+	utils.FormatOutput("Functions with named returns", fmt.Sprintf("parameter1: %d, parameter2: %d", paramter1, paramter2), fmt.Sprintf("result: %d, error: %s", value, err))
+
+	value = funcWithNameReturnShadow()
+	utils.FormatOutput("Functions with name return shadow", "No parameters", fmt.Sprintf("result: %d", value))
 }
